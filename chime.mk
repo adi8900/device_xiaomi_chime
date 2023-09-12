d0db7422cde02c866d594d5033bf6203f9d8e638
Michael Bestas <mkbestas@lineageos.org>
2023-09-12 13:42:51 +0200

    chime: Build GPS HAL from source

--- chime.mk
+++ chime.mk
@@ -173,8 +173,20 @@ PRODUCT_PACKAGES += \
 
 # GPS
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.1.vendor \
-    android.hardware.gnss@2.1.vendor
+    android.hardware.gnss@2.1-impl-qti \
+    android.hardware.gnss@2.1-service-qti \
+    android.hardware.gnss@2.1.vendor \
+    gnss@2.0-base.policy \
+    gnss@2.0-xtra-daemon.policy \
+    gnss@2.0-xtwifi-client.policy \
+    gnss@2.0-xtwifi-inet-agent.policy \
+    gps.conf \
+    flp.conf \
+    libbatching \
+    libgeofencing \
+    libgnss \
+    libgnsspps \
+    libloc_socket
 
 # Health
 PRODUCT_PACKAGES += \
